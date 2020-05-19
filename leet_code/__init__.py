427b8190d66a227d17eae0e35001b1cc57a45bdc
645187919 <645187919@qq.com>
2020-05-19 16:28:22 +0800

    Initial commit

---
+++ leet_code/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2020/5/19 16:23 
+# @Author : magician 
+# @File : __init__.py.py 
+# @Software: PyCharm
---
+++ leet_code/part_1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2020/5/19 16:24 
+# @Author : magician 
+# @File : part_1.py 
+# @Software: PyCharm
+
+
