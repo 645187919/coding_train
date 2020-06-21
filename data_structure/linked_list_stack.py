610b2a60387bb2adcce7de16141f96ee73cecef3
645187919 <645187919@qq.com>
2020-06-21 21:46:13 +0800

    add data structure project

---
+++ data_structure/linked_list_stack.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2020/6/21 21:34 
+# @Author : magician 
+# @File : linked_list_stack.py 
+# @Software: PyCharm
+
+from data_structure.linked_list import Linked_list
+class Linked_list_stack(Linked_list):
+
+    def __init__(self):
+        self.list=Linked_list()
+
+    # def get_size(self):
+    def push(self,value):
+        self.list.add_first(value)
+
+
+    def pop(self):
+        return self.list.remove_first()
+
+    def peak(self):
+        return self.list.get_first()
+
+
+if __name__ == '__main__':
+    ls = Linked_list_stack()
+    print(ls.push(1))
+    print(ls.push(2))
+    print(ls.peak())
+    print(ls.pop())
+    print(ls.peak())
+
+
