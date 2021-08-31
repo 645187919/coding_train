2b21f319082f6723469f8f8245dba3e75b40703c
645187919 <645187919@qq.com>
2021-08-31 20:05:40 +0800

    add data structure project

--- sort_algorithm/__init__.py
+++ sort_algorithm/__init__.py
@@ -6,27 +6,10 @@
 # @Software: PyCharm
 
 
-def quick_sort(alist,start,end):
+a = 1
+b = 2
+h = ""
 
-    if start>end:
-        return
-    #基准元素
-    mind=alist[start]
-    low=start
-    high=end
-    while low<high:
-        while low<high and mind<=alist[high]:
-            high-=1
-        alist[high],alist[low]=alist[low],alist[high]
+h = "变量1" if a>b else "变量2"
 
-        while low<high and mind>=alist[low]:
-            low+=1
-        alist[high],alist[low]=alist[low],alist[high]
-
-    alist[low]=mind
-    quick_sort(alist,start,low-1)
-    quick_sort(alist,low+1,end)
-
-alist = [54,26,93,17,77,31,44,55,20]
-quick_sort(alist,0,len(alist)-1)
-print(alist)
+print(h)
