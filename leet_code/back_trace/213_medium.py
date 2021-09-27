b5b14270f393e887b3b627f0873379819ee8395a
645187919 <645187919@qq.com>
2021-09-27 20:32:56 +0800

    add data structure project

---
+++ leet_code/back_trace/213_medium.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2021-09-27 20:29 
+# @Author : magician 
+# @File : 213_medium.py 
+# @Software: PyCharm
+
+
+# 213. 打家劫舍 II
+# 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+# 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+
+# 示例 1：
+#
+# 输入：nums = [2,3,2]
+# 输出：3
+# 解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+# 示例 2：
+#
+# 输入：nums = [1,2,3,1]
+# 输出：4
+# 解释：你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+# 偷窃到的最高金额 = 1 + 3 = 4 。
+# 示例 3：
+#
+# 输入：nums = [0]
+# 输出：0
+#
+#
+# 提示：
+#
+# 1 <= nums.length <= 100
+# 0 <= nums[i] <= 1000
+
+#思路：正常打家劫舍的实现的改变，这里由于牵涉到房子是环形，所以需要做两种情况的分析，偷第一间不偷最后一间，
+#不偷第一间偷最后一间。所以
+
+#详情参考：https://leetcode-cn.com/problems/house-robber-ii/solution/dong-tai-gui-hua-by-javaniuniu-2/
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        #打家劫舍1的代码
+        def max_rob(nums: List[int]) -> int:
+            #dp[i]：考虑偷i个房子所能拿到的最大金额；
+            #状态转移方程：dp[i]=max(dp[i-1],dp[i-2]+nums[i-1])
+            #初始化：dp[0]=0,dp[1]=num[0]
+            #遍历
+            n_len=len(nums)
+
+            dp=[0 for i in range(n_len+1)]
+            dp[0]=0
+            dp[1]=nums[0]
+            for i in range(2,n_len+1):
+                dp[i]=max(dp[i-1],dp[i-2]+nums[i-1])
+
+            return dp[-1]
+
+        #新增的代码
+        if not nums:
+            return 0
+        if len(nums)==1:
+            return nums[0]
+
+        num1=nums[0:len(nums)-1]
+        num2=nums[1:]
+        return max(max_rob(num1),max_rob(num2))
