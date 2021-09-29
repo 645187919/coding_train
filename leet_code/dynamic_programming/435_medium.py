29976253ab690a80405ea3239f73b4c3c32e7939
645187919 <645187919@qq.com>
2021-09-29 22:03:43 +0800

    add data structure project

---
+++ leet_code/dynamic_programming/435_medium.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2021-09-29 22:03 
+# @Author : magician 
+# @File : 435_medium.py 
+# @Software: PyCharm
+
+# 435. 无重叠区间
+# 给定一个区间的集合，找到需要移除区间的最小数量，使剩余区间互不重叠。
+#
+# 注意:
+#
+# 可以认为区间的终点总是大于它的起点。
+# 区间 [1,2] 和 [2,3] 的边界相互“接触”，但没有相互重叠。
+# 示例 1:
+#
+# 输入: [ [1,2], [2,3], [3,4], [1,3] ]
+#
+# 输出: 1
+#
+# 解释: 移除 [1,3] 后，剩下的区间没有重叠。
+# 示例 2:
+#
+# 输入: [ [1,2], [1,2], [1,2] ]
+#
+# 输出: 2
+#
+# 解释: 你需要移除两个 [1,2] 来使剩下的区间没有重叠。
+# 示例 3:
+#
+# 输入: [ [1,2], [2,3] ]
+#
+# 输出: 0
+#
+# 解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
+
+# 思路：https://leetcode-cn.com/problems/non-overlapping-intervals/solution/chuan-shang-yi-fu-wo-jiu-bu-ren-shi-ni-liao-lai-2/
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+
+        #dp[i]:代表长度为i的列表的最大子序列的长度
+
+        dp=[1 for i in range(len(intervals))]
+        intervals.sort(key=lambda a: a[0])
+
+        for i in range(len(intervals)):
+            for j in range(i-1,-1,-1):
+                if intervals[i][0]>=intervals[j][1]:
+                    dp[i]=max(dp[i],dp[j]+1)
+                    break
+
+        return len(intervals)-max(dp)
+
